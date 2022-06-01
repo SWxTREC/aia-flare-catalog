c6bde83adaa8700b8b9fdbf945cf6dd1ca77d639
Kiera van der Sande <kiera.vds@gmail.com>
2022-06-01 12:39:34 -0600

    new function to plot goes flare

--- helper.py
+++ helper.py
@@ -32,3 +32,28 @@ def plot_flare(flare,aia_times,aia_data,window):
         ax[j].legend()
         ax[j].grid()
     ax[0].set_title('SHARP '+str(flare['SHARP'])+': GOES flare '+str(int(flare['goes_flare_ind']))+' '+str(flare['CMX'])+str(flare['CMX_VALUE']/10)+' predicted '+str(flare['ert_pred_CMX'])+str(flare['ert_pred_intensity'])[:4])
+
+def plot_flare_goes(flare,aia_times,aia_data,window):
+    lams = ['193','171','304','1600','131','94']
+    colors = ['tab:blue','tab:orange','tab:green','tab:red','tab:purple','tab:brown']
+
+    start = flare['mean_start_time']-timedelta(minutes=window)
+    end = flare['mean_end_time']+timedelta(minutes=window)
+    fig,ax = plt.subplots(6,1,figsize=(10,10))
+    for j in range(len(lams)):
+        lam = lams[j]
+        color = colors[j]   
+
+        sum_intensity = np.array(aia_data[j])
+        times = np.array(aia_times[j])
+        sum_intensity = sum_intensity[np.logical_and(times>np.datetime64(start),times<np.datetime64(end))]
+        times = times[np.logical_and(times>np.datetime64(start),times<np.datetime64(end))]
+
+        ax[j].axvspan(flare['mean_start_time'],flare['mean_end_time'],alpha=0.3,color='blue')
+        ax[j].plot(times,sum_intensity,'-',color=color,label=lam, alpha=1)
+        ax[j].set_xlim(start,end)
+        ax[j].set_ylim(0.9*np.min(sum_intensity),1.1*np.max(sum_intensity))
+        ax[j].legend()
+        ax[j].grid()
+    ax[0].set_title('SHARP '+str(flare['SHARP'])+': GOES flare '+str(int(flare['flare_ind']))+' '+str(flare['CMX'])+str(flare['CMX_VALUE']/10))
+    print('start:',flare['mean_start_time'],', end:',flare['mean_end_time'])

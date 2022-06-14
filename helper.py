4b075e719c8146abb3b3c7dda64bf2aae9a51ae4
Kiera van der Sande <kiera.vds@gmail.com>
2022-06-13 18:43:15 -0600

    added 3rd goal

--- helper.py
+++ helper.py
@@ -35,6 +35,35 @@ def plot_flare(flare,aia_times,aia_data,window):
         ax[j].grid()
     ax[0].set_title('SHARP '+str(flare['SHARP'])+': GOES flare '+str(int(flare['goes_flare_ind']))+' '+str(flare['CMX'])+str(flare['CMX_VALUE']/10)+' predicted '+str(flare['ert_pred_CMX'])+str(flare['ert_pred_intensity'])[:4])
 
+def plot_flare_nogoes(flare,aia_times,aia_data,window):
+    lams = ['193','171','304','1600','131','94']
+    colors = ['tab:blue','tab:orange','tab:green','tab:red','tab:purple','tab:brown']
+
+    start = flare['mean_start_time']-timedelta(minutes=window)
+    end = flare['mean_end_time']+timedelta(minutes=window)
+    fig,ax = plt.subplots(6,1,figsize=(10,10))
+    for j in range(len(lams)):
+        lam = lams[j]
+        color = colors[j]   
+
+        sum_intensity = np.array(aia_data[j])
+        times = np.array(aia_times[j])
+        sum_intensity = sum_intensity[np.logical_and(times>np.datetime64(start),times<np.datetime64(end))]
+        times = times[np.logical_and(times>np.datetime64(start),times<np.datetime64(end))]
+        if len(times) > 0:
+            ax[j].plot(times,sum_intensity,'-',color=color,label=lam, alpha=1)
+            ax[j].set_ylim(0.9*np.min(sum_intensity),1.1*np.max(sum_intensity))
+        
+        if pd.notna(flare[lam+'_peak_time']): 
+            ax[j].plot(flare[lam+'_peak_time'],flare[lam+'_magnitude'],'.k',label='_')
+            ax[j].plot(flare[lam+'_start_time'],flare[lam+'_magnitude']-flare[lam+'_prominence'],'xk',label='_')
+            ax[j].axvline(flare[lam+'_end_time'],color='k',ls='--',label='_')
+        ax[j].set_xlim(start,end)
+        ax[j].legend()
+        ax[j].grid()
+    ax[0].set_title('SHARP '+str(flare['SHARP'])+': predicted '+str(flare['ert_pred_CMX'])+str(flare['ert_pred_intensity'])[:4]+ ', mean start time:'+str(flare['mean_start_time']))
+
+
 def plot_flare_goes(flare,aia_times,aia_data,window):
     lams = ['193','171','304','1600','131','94']
     colors = ['tab:blue','tab:orange','tab:green','tab:red','tab:purple','tab:brown']
